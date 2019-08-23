2c7ed92b42d57a99b1ead654e512c8ce0fef0366
vitorroxo <vitor@mobi2buy.com>
2019-08-23 18:00:07 -0300

    Finish app

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 export default props => (
-    <nav className="navbar navbar-inverse bg-inverse">
-        <div className="container">
-            <div className="navbar-header">
-                <a href="#" className="navbar-brand">
-                    <i className="fa fa-calendar-check-o"> TodoApp </i>
+    <nav className='navbar navbar-inverse bg-inverse'>
+        <div className='container'>
+            <div className='navbar-header'>
+                <a className='navbar-brand' href='#'>
+                    <i className='fa fa-calendar-check-o'></i> TodoApp
                 </a>
             </div>
 
-            <div id="navbar" className="navbar-collapse collapse">
-                <ul className="nav navbar-nav">
-                    <li><a href="#/todos">Tarefas</a></li>
-                    <li><a href="#/about">Sobre</a></li>
+            <div id='navbar' className='navbar-collapse collapse'>
+                <ul className='nav navbar-nav'>
+                    <li><a href='#/todos'>Tarefas</a></li>
+                    <li><a href='#/about'>Sobre</a></li>
                 </ul>
             </div>
         </div>
