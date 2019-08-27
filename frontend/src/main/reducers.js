44e6429e6fa2d723d2225a3d6b7c2021c14fee2f
vitorroxo <vitor@mobi2buy.com>
2019-08-27 19:05:57 -0300

    Finish integration with redux

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,8 @@
 import { combineReducers } from 'redux'
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-    todo: () => ({ 
-        descrption: "Ler livro.",
-        list: [{
-            _id: 1,
-            description: 'Pagar fatura do cartão.',
-            done: true
-        }, {
-            _id: 2,
-            description: 'Reunião com a equipe às 10h',
-            done: false    
-        }, {
-            _id: 3,
-            description: 'Consulta médicas na terça depois do almoço.',
-            done: false    
-        }]
-     })
+    todo: todoReducer
 })
 
 export default rootReducer
